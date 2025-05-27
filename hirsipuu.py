a9541c031e0c66ce3d684543637cec339b3f4e2d
Timo Waali <waali.timo@gmail.com>
2025-05-27 06:51:38 +0300

    aloittelua

---
+++ hirsipuu.py
@@ -0,0 +1,14 @@
+def lataa_sanat(tiedoston_nimi): #lataa sanat tiedostosta
+    pass
+    
+
+def arvaa_kirjain(sana, arvaukset): #palauttaa listan jossa näkyy arvatut kirjaimet ja muiden tilalla alaviiva
+    pass
+    
+
+def hirsipuu(): #pelin alustus, silmukka, tarkistukset(oikea kirjain-väärä kirjain), voitto/häviötilanteet
+    pass
+
+
+# Pelin käynnistys:
+hirsipuu()
